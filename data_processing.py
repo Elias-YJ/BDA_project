245736bb6e281252875be4d86c0945acc3194068
Elias-YJ <elias.yla-jarkko@live,fi>
2021-11-15 12:32:42 +0200

    Convert all features to numeric

--- data_processing.py
+++ data_processing.py
@@ -5,6 +5,11 @@ def add_split_columns(df):
     df = df.copy()
     df['RestingECG_ST'] = (df['RestingECG'] == 'ST').astype(int)
     df['RestingECG_LVH'] = (df['RestingECG'] == 'LVH').astype(int)
+
+    df['ChestPainATA'] = (df['ChestPainType'] == 'ATA').astype(int)
+    df['ChestPainNAP'] = (df['ChestPainType'] == 'NAP').astype(int)
+    df['ChestPainASY'] = (df['ChestPainType'] == 'ASY').astype(int)
+    df['ChestPainTA'] = (df['ChestPainType'] == 'TA').astype(int)
     return df
 
 
@@ -12,15 +17,17 @@ def add_mapped_columns(df):
     df = df.copy()
     st_map = {'Up': 1, 'Flat': 0, 'Down': -1}
     angina_map = {'Y': 1, 'N': 0}
+    gender_map = {'F': 0, 'M': 1}
 
     df['ST_Slope'] = df['ST_Slope'].map(st_map)
     df['ExerciseAngina'] = df['ExerciseAngina'].map(angina_map)
+    df['Gender'] = df['Sex'].map(gender_map)
     return df
 
 
 def drop_unused_columns(df):
     df = df.copy()
-    df = df.drop(['RestingECG'], axis=1)
+    df = df.drop(['RestingECG', 'ChestPainType', 'Sex'], axis=1)
     return df
 
 
