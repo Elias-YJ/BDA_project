cf8b9c14d4fa77293c20f8f0b09b082515c1689a
Elias-YJ <elias.yla-jarkko@live,fi>
2021-11-15 11:29:14 +0200

    Add processing functions WIP

--- data_processing.py
+++ data_processing.py
@@ -2,11 +2,26 @@ import pandas as pd
 
 
 def add_split_columns(df):
-    return
+    df = df.copy()
+    df['RestingECG_ST'] = (df['RestingECG'] == 'ST').astype(int)
+    df['RestingECG_LVH'] = (df['RestingECG'] == 'LVH').astype(int)
+    return df
 
 
 def add_mapped_columns(df):
-    return
+    df = df.copy()
+    st_map = {'Up': 1, 'Flat': 0, 'Down': -1}
+    angina_map = {'Y': 1, 'N': 0}
+
+    df['ST_Slope'] = df['ST_Slope'].map(st_map)
+    df['ExerciseAngina'] = df['ExerciseAngina'].map(angina_map)
+    return df
+
+
+def drop_unused_columns(df):
+    df = df.copy()
+    df = df.drop(['RestingECG'], axis=1)
+    return df
 
 
 def process(raw='data/heart.csv', output='data/model_input.csv'):
@@ -14,4 +29,6 @@ def process(raw='data/heart.csv', output='data/model_input.csv'):
     data = raw_data.copy()
     data = add_split_columns(data)
     data = add_mapped_columns(data)
+    data = drop_unused_columns(data)
     data.to_csv(output)
+    return data
